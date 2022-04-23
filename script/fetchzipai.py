f1bccaed583b204bf92c230dbbaa95532520c098
Chen renjie <1332679637@qq.com>
2022-04-24 00:43:50 +0800

    update photo

--- script/fetchzipai.py
+++ script/fetchzipai.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from bs4.element import Tag, NavigableString
 import os, time, random
+import json
 SAVE_PATH = "./"
 ROOT = "https://99zipai.com"
 HEADERS = {
@@ -11,6 +12,7 @@ HEADERS = {
 
 
 def article(url):
+    aid = url.split("/")[-1].split(".")[0]
     r = requests.get(ROOT+url, headers=HEADERS)
     if r.status_code != 200:
         print(r.text)
@@ -23,10 +25,13 @@ def article(url):
     for idx, ele in enumerate(div.contents):
         if isinstance(ele, Tag):
             if ele.name == "img":
+                src = ele.get("src")
+                if not src.startswith("http"):
+                    continue
                 if isinstance(content[-1], str):
-                    content.append([ele.get("src")])
+                    content.append([src])
                 else:
-                    content[-1].append(ele.get("src"))
+                    content[-1].append(src)
         elif isinstance(ele, NavigableString):
             t = ele.strip()
             if t != "":
@@ -34,23 +39,47 @@ def article(url):
                 content.append(t)
         else:
             print("Unknown :", type(ele), ele)
-    print(content)
+    with open(f"photo/data/zipai/{aid}.json", "w") as f:
+        json.dump({"data": content}, f)
+    # print(aid, title)
     
 def worklist(uid):
-    url = f"{ROOT}/my/{uid}/"
+    # url = f"{ROOT}/my/{uid}/"
+    url = f"https://99zipai.com/e/space/ulist.php?page=0&mid=1&line=80&tempid=10&orderby=&myorder=0&userid={uid}"
     r = requests.get(url, headers=HEADERS)
     assert r.status_code == 200, r.text
     r.encoding = r.apparent_encoding
-    # print(r.text)
     soup = BeautifulSoup(r.text, "html.parser")
-    h2 = soup.find(name="h2", attrs={"itemprop": "name"})
-    print(h2.text)
+    total = soup.find("a", attrs={"class": "sh_1"}).span.text.strip()
+
+    user = soup.find(name="h2", attrs={"itemprop": "name"}).text.strip()
+    print(total, user)
     ul = soup.find(name="ul", attrs={"class": "ul_author_list cl"})
     for idx, li in enumerate(ul.findAll("li")):
         a = li.a
-        print(a.text, ROOT+"/"+a.get("href"))
+        yield a.text, a.get("href")
 
+def main(author):
+    data = []
+    for title, href in worklist(author):
+        if input(f"Download {title}({ROOT+href})[y/n]?: ")[0].lower() == "y":
+            article(href)
+            data.append([href.split("/")[-1].split(".")[0], title])
+    print(data[::-1])
+        
 
-# worklist("93")
+# worklist("7744")
 # article("/selfies/202105/121680.html")
-article("/selfies/202105/121680.html")
+# article("/selfies/202105/121680.html")
+
+for uid in range(1000, 1100):
+    for title, href in worklist(str(uid)):
+        print(title, ROOT+href)
+    if input(f"Continue [y/n]?: ")[0].lower() == "y":
+        pass
+    else:
+        break
+
+
+# article("/selfies/201807/68971.html")
+# main("12765")
